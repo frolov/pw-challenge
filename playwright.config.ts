18495d5307e50a1c89198dc585b90b8fe2332e74
Vladimir Frolov <vfrolov@mbp.local>
2026-03-09 22:01:43 -0700

    Updated test for c1: removed for loop, added fixture to continue test run on failure, enable parallelization and reporting clarity

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 3 : 6,
+  workers: process.env.CI ? 3 : 1,
   reporter: [['html', { open: 'never' }], ['dot']],
   timeout: 2 * 60 * 1000,
   expect: {
--- tests/flaky.spec.ts
+++ tests/flaky.spec.ts
@@ -3,28 +3,35 @@ import { expect, test } from '@playwright/test';
 const testEmailAddress = 'test1@example.com'
 const testPassword = 'password1'
 
-// Fix the below scripts to work consistently and do not use static waits. Add proper assertions to the tests
+//Fix the below scripts to work consistently and do not use static waits. Add proper assertions to the tests
 // Login 3 times sucessfully
 
 // VF: A simple solution based on observations of how the DOM structure changes after login
-test('Login multiple times sucessfully @c1', async ({ page }) => {
-  const submitButton = page.locator('#submitButton');
-  const submitButtonLoading = page.locator('.submit-btn.loading');
-  const successMessage = page.locator(`#successMessage`);
-  const visibleSuccessMessage = page.locator('.success-message.show');
+// VF: Update - removed for loop, added fixture to continue test run on failure, 
+// enable parallelization and reporting clarity
+const testUsers = [
+  { id: 1, email: 'test1@example.com', pass: 'password1'},
+  { id: 2, email: 'test2@example.com', pass: 'password2'},
+  { id: 3, email: 'test3@example.com', pass: 'password3'},
+]
+for (const user of testUsers) {
+  test(`Login multiple times, sucessfully for user ${user.id} @c1`, async ({ page }) => {
+    const submitButton = page.locator('#submitButton');
+    const submitButtonLoading = page.locator('.submit-btn.loading');
+    const successMessage = page.locator(`#successMessage`);
+    const visibleSuccessMessage = page.locator('.success-message.show');
 
-  await page.goto('/');
-  await page.locator(`//*[@href='/challenge1.html']`).click();
+    await page.goto('/');
+    await page.locator(`//*[@href='/challenge1.html']`).click();
 
-  // Login multiple times
-  for (let i = 1; i <= 3; i++) {
+    // Login multiple times
     // VF: Check Sign In button is shown in initial state only
     await expect(submitButton).toBeVisible
     await expect(visibleSuccessMessage).toBeHidden()
     await expect(submitButtonLoading).toBeHidden()
 
-    await page.locator('#email').fill(`${i}${testEmailAddress}`);
-    await page.locator('#password').fill(`${i}${testPassword}`);
+    await page.locator('#email').fill(user.email);
+    await page.locator('#password').fill(user.pass);
     await submitButton.click();
 
     // VF: Check the transition from Sign In button in loading state to success message in show state
@@ -32,12 +39,12 @@ test('Login multiple times sucessfully @c1', async ({ page }) => {
     await expect(visibleSuccessMessage).toBeAttached()
 
     await expect(successMessage).toContainText('Successfully submitted!');
-    await expect(successMessage).toContainText(`Email: ${i}${testEmailAddress}`);
-    await expect(successMessage).toContainText(`Password: ${i}${testPassword}`);
+    await expect(successMessage).toContainText(`Email: ${user.email}`);
+    await expect(successMessage).toContainText(`Password: ${user.pass}`);
 
     await expect(submitButton).toBeVisible()
-  }
-});
+  });
+}
 
 // Login and logout successfully with animated form and delayed loading
 // VF: I don't really like this solution because it relies on an implicit wait for the animation part,
